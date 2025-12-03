7aec785ea2d059f407cf996943a3f90745a819bd
ronli <ronli@Ubuntu.myguest.virtualbox.org>
2025-12-03 16:11:02 -0500

    Assignment 4 part 1 submission.

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -20,6 +20,9 @@ struct thread_data{
      * if an error occurred.
      */
     bool thread_complete_success;
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
+    pthread_mutex_t *mutex;
 };
 
 
